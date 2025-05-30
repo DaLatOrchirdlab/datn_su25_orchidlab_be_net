ab224e8e32d0f4dadf6c9dfff16d8778dfb10d1e
ThanhIsHereee <nquangthanh88@gmail.com>
2025-05-31 04:29:47 +0700

    add infastructure

---
+++ orchid-backend-net/orchid-backend-net.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using orchid_backend_net.Domain.Common.Interfaces;
+using orchid_backend_net.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orchid_backend_net.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OrchidDbContext>((sp, options) =>
+            {
+                options.UseSqlServer(
+                    configuration.GetConnectionString("Server"),
+                    b =>
+                    {
+                        b.MigrationsAssembly(typeof(OrchidDbContext).Assembly.FullName);
+                        b.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+                    });
+                options.UseLazyLoadingProxies();
+
+
+            });
+
+            services.AddScoped<IUnitOfWork>(provider => (IUnitOfWork)provider.GetRequiredService<OrchidDbContext>());
+
+            return services;
+        }
+    }
+}
