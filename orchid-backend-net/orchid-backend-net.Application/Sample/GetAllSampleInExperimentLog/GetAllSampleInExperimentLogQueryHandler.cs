f343c1c684f93f663b6dba0716543d764f3084ca
ThanhIsHereee <nquangthanh88@gmail.com>
2025-06-06 01:18:17 +0700

    Add: Sample API

---
+++ orchid-backend-net/orchid-backend-net.Application/Sample/GetAllSampleInExperimentLog/GetAllSampleInExperimentLogQueryHandler.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using MediatR;
+using orchid_backend_net.Application.Common.Pagination;
+using orchid_backend_net.Domain.Entities;
+using orchid_backend_net.Domain.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orchid_backend_net.Application.Sample.GetAllSampleInExperimentLog
+{
+    public class GetAllSampleInExperimentLogQueryHandler : IRequestHandler<GetAllSampleInExperimentLogQuery, PageResult<SampleDTO>>
+    {
+        private readonly ISampleRepository _sampleRepository;
+        private readonly ILinkedRepository _linkedRepository;
+        private readonly IMapper _mapper;
+        public GetAllSampleInExperimentLogQueryHandler(ISampleRepository sampleRepository, ILinkedRepository linkedRepository, IMapper mapper)
+        {
+            _sampleRepository = sampleRepository;
+            _linkedRepository = linkedRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<PageResult<SampleDTO>> Handle(GetAllSampleInExperimentLogQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var listSample = await this._linkedRepository.FindAllAsync(x => x.ExperimentLogID.Equals(request.ExperimentLogID) && x.Status == true, request.PageNumber, request.PageSize, cancellationToken);
+                List<orchid_backend_net.Domain.Entities.Sample> result = new List<Domain.Entities.Sample>();
+                foreach (var sample in listSample)
+                {
+                    var a = _sampleRepository.FindAsync(x => x.ID.Equals(sample.SampleID), cancellationToken);
+                    if (a != null)
+                        result.Add(await a);
+                }
+                return PageResult<SampleDTO>.Create(
+                    totalCount: result.Count,
+                    pageCount: result.Count,
+                    pageNumber: request.PageNumber,
+                    pageSize: request.PageSize,
+                    data: result.MapToSampleDTOList(_mapper)
+                    );
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception($"{ex.Message}");
+            }
+        }
+    }
+}
---
+++ orchid-backend-net/orchid-backend-net.Application/Sample/GetSampleInfor/GetSampleInforQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MediatR;
+using orchid_backend_net.Domain.Common.Exceptions;
+using orchid_backend_net.Domain.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orchid_backend_net.Application.Sample.GetSampleInfor
+{
+    public class GetSampleInforQueryHandler : IRequestHandler<GetSampleInforQuery, SampleDTO>
+    {
+        private readonly ISampleRepository _sampleRepository;
+        private readonly IMapper _mapper;
+        public GetSampleInforQueryHandler(ISampleRepository sampleRepository, IMapper mapper)
+        {
+            _sampleRepository = sampleRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<SampleDTO> Handle(GetSampleInforQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await this._sampleRepository.FindAsync(x => x.ID.Equals(request.ID), cancellationToken);
+                if (result == null)
+                    throw new NotFoundException($"Not found sample with ID:{request.ID} in the system.");
+                return result.MapToSampleDTO(_mapper);
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception($"{ex.Message}");
+            }
+        }
+    }
+}
