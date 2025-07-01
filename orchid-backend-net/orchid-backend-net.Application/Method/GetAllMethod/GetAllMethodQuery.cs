57c26dc899851a118887e40eb9dc0ab5038c389d
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2025-07-01 15:45:26 +0700

    feat: method get all api

--- orchid-backend-net/orchid-backend-net.Application/Method/GetAllMethod/GetAllMethodQuery.cs
+++ orchid-backend-net/orchid-backend-net.Application/Method/GetAllMethod/GetAllMethodQuery.cs
@@ -1,11 +1,9 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
 using orchid_backend_net.Application.Common.Interfaces;
 using orchid_backend_net.Application.Common.Pagination;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using orchid_backend_net.Domain.Common.Exceptions;
+using orchid_backend_net.Domain.IRepositories;
 
 namespace orchid_backend_net.Application.Method.GetAllMethod
 {
@@ -14,10 +12,41 @@ namespace orchid_backend_net.Application.Method.GetAllMethod
         public int PageNumber { get; set; }
         public int PageSize { get; set; }
         public GetAllMethodQuery() { }
-        public GetAllMethodQuery(int pageNumber, int pageSize) 
+        public GetAllMethodQuery(int pageNumber, int pageSize)
         {
             PageSize = pageSize;
             PageNumber = pageNumber;
+        }
+    }
+
+    internal class GetAllMethodQueryHandler : IRequestHandler<GetAllMethodQuery, PageResult<MethodDTO>>
+    {
+        private readonly IMethodRepository _methodRepository;
+        private readonly IMapper _mapper;
+        public GetAllMethodQueryHandler(IMethodRepository methodRepository, IMapper mapper)
+        {
+            _methodRepository = methodRepository;
+            _mapper = mapper;
+        }
+        public async Task<PageResult<MethodDTO>> Handle(GetAllMethodQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var list = await this._methodRepository.FindAllAsync(x => x.Status == true, request.PageNumber, request.PageSize, cancellationToken);
+                if (list.Count() == 0)
+                    throw new NotFoundException("Not found any method in the system.");
+                return PageResult<MethodDTO>.Create(
+                    totalCount: list.TotalCount,
+                    pageCount: list.PageSize,
+                    pageNumber: request.PageNumber,
+                    pageSize: request.PageSize,
+                    data: list.MapToMethodDTOList(_mapper)
+                    );
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{ex.Message}");
+            }
         }
     }
 }
