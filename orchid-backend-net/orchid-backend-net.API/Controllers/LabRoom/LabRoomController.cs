e4c506db35cc154fe852c96e252004c8ad2f82a8
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2025-07-13 20:14:06 +0700

    feat: full crud api for lab room

--- orchid-backend-net/orchid-backend-net.API/Controllers/LabRoom/LabRoomController.cs
+++ orchid-backend-net/orchid-backend-net.API/Controllers/LabRoom/LabRoomController.cs
@@ -3,8 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using orchid_backend_net.API.Controllers.ResponseTypes;
 using orchid_backend_net.Application.Common.Pagination;
 using orchid_backend_net.Application.LabRoom;
+using orchid_backend_net.Application.LabRoom.CreateLabRoom;
+using orchid_backend_net.Application.LabRoom.DeleteLabRoom;
 using orchid_backend_net.Application.LabRoom.GetAllLabRoom;
 using orchid_backend_net.Application.LabRoom.GetLabRoomInfor;
+using orchid_backend_net.Application.LabRoom.UpdateLabRoom;
 using System.Net.Mime;
 
 namespace orchid_backend_net.API.Controllers.LabRoom
@@ -28,7 +31,7 @@ namespace orchid_backend_net.API.Controllers.LabRoom
         {
             try
             {
-                var result = await this._sender.Send(new GetAllLabRoomQuery(pageNumber, pageSize), cancellationToken);
+                var result = await sender.Send(new GetAllLabRoomQuery(pageNumber, pageSize), cancellationToken);
                 logger.LogInformation("Received GET request at {Time}", DateTime.UtcNow);
                 return Ok(new JsonResponse<PageResult<LabRoomDTO>>(result));
             }
@@ -53,13 +56,88 @@ namespace orchid_backend_net.API.Controllers.LabRoom
         {
             try
             {
-                var result = await this._sender.Send(new GetLabRoomInforQuery(id), cancellationToken);
+                var result = await sender.Send(new GetLabRoomInforQuery(id), cancellationToken);
                 logger.LogInformation("Received GET request at {Time}", DateTime.UtcNow);
                 return Ok(new JsonResponse<LabRoomDTO>(result));
             }
             catch (Exception ex)
             {
                 logger.LogInformation(ex, "Error occurred while processing GET request at {Time}", DateTime.UtcNow);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(JsonResponse<string>), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(JsonResponse<string>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<JsonResponse<string>>> CreateLabRoom(
+            [FromBody] CreateLabRoomCommand command,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await sender.Send(command, cancellationToken);
+                logger.LogInformation("Received POST request at {Time}", DateTime.UtcNow);
+                return Ok(new JsonResponse<string>(result));
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error occurred while processing POST request at {Time}", DateTime.UtcNow);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(JsonResponse<string>), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(JsonResponse<string>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<JsonResponse<string>>> UpdateLabRoom(
+            [FromBody] UpdateLabRoomCommand command,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await sender.Send(command, cancellationToken);
+                logger.LogInformation("Received PUT request at {Time}", DateTime.UtcNow);
+                return Ok(new JsonResponse<string>(result));
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error occurred while processing PUT request at {Time}", DateTime.UtcNow);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(JsonResponse<string>), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(JsonResponse<string>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<JsonResponse<string>>> DeleteLabRoom(
+            [FromBody] DeleteLabRoomCommand command, 
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await sender.Send(command, cancellationToken);
+                logger.LogInformation("Received DELETE request at {Time}", DateTime.UtcNow);
+                return Ok(new JsonResponse<string>(result));
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error occurred while processing DELETE request at {Time}", DateTime.UtcNow);
                 return BadRequest(ex.Message);
             }
         }
