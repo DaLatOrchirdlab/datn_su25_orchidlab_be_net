12922d64713e816b70223b41ca818ad610f55a7f
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2025-07-13 19:32:49 +0700

    feat: lab room api

---
+++ orchid-backend-net/orchid-backend-net.API/Controllers/LabRoom/LabRoomController.cs
@@ -0,0 +1,67 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using orchid_backend_net.API.Controllers.ResponseTypes;
+using orchid_backend_net.Application.Common.Pagination;
+using orchid_backend_net.Application.LabRoom;
+using orchid_backend_net.Application.LabRoom.GetAllLabRoom;
+using orchid_backend_net.Application.LabRoom.GetLabRoomInfor;
+using System.Net.Mime;
+
+namespace orchid_backend_net.API.Controllers.LabRoom
+{
+    [Route("api/labroom")]
+    [ApiController]
+    public class LabRoomController(ISender sender, ILogger<LabRoomController> logger) : BaseController(sender)
+    {
+        [HttpGet]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(JsonResponse<LabRoomDTO>), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(JsonResponse<LabRoomDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<JsonResponse<PageResult<LabRoomDTO>>>> GetAllLabRoom(
+            [FromQuery] int pageNumber,
+            [FromQuery] int pageSize,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await this._sender.Send(new GetAllLabRoomQuery(pageNumber, pageSize), cancellationToken);
+                logger.LogInformation("Received GET request at {Time}", DateTime.UtcNow);
+                return Ok(new JsonResponse<PageResult<LabRoomDTO>>(result));
+            }
+            catch (Exception ex)
+            {
+                logger.LogInformation(ex, "Error occurred while processing GET request at {Time}", DateTime.UtcNow);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(JsonResponse<LabRoomDTO>), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(JsonResponse<LabRoomDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<JsonResponse<LabRoomDTO>>> GetAllLabRoom(
+            [FromRoute] string id,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await this._sender.Send(new GetLabRoomInforQuery(id), cancellationToken);
+                logger.LogInformation("Received GET request at {Time}", DateTime.UtcNow);
+                return Ok(new JsonResponse<LabRoomDTO>(result));
+            }
+            catch (Exception ex)
+            {
+                logger.LogInformation(ex, "Error occurred while processing GET request at {Time}", DateTime.UtcNow);
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
