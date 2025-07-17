a9215bee4700b51835827d5d5f13b4097bbf7401
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2025-07-18 02:27:35 +0700

    fix: assign and attribute dto

--- orchid-backend-net/orchid-backend-net.Application/TaskAttribute/TaskAttributeDTO.cs
+++ orchid-backend-net/orchid-backend-net.Application/TaskAttribute/TaskAttributeDTO.cs
@@ -7,19 +7,20 @@ namespace orchid_backend_net.Application.TaskAttribute
     public class TaskAttributeDTO : IMapFrom<TaskAttributes>
     {
         public string ID { get; set; }
-        //public string Name { get; set; }
+        public string Name { get; set; }
         public string Description { get; set; }
+        public string MeasurementUnit { get; set; }
         public double Value { get; set; }
-        //public enum Unit
         public bool Status { get; set; }
 
-        public TaskAttributeDTO(string iD, string description, double value, bool status)
+        public TaskAttributeDTO(string id, string name, string description, string measurementUnit, double value, bool status)
         {
-            ID = iD;
-            //Name = name;
+            ID = id;
+            Name = name;
             Description = description;
             Value = value;
             Status = status;
+            MeasurementUnit = measurementUnit;
         }
         public TaskAttributeDTO() { }
         public void Mapping(Profile profile)
