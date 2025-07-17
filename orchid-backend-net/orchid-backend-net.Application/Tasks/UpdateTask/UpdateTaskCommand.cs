f6b8dfb2bd5100ae4eb92f8ce7f624836aead142
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2025-07-17 23:33:10 +0700

    fix: unsed

--- orchid-backend-net/orchid-backend-net.Application/Tasks/UpdateTask/UpdateTaskCommand.cs
+++ orchid-backend-net/orchid-backend-net.Application/Tasks/UpdateTask/UpdateTaskCommand.cs
@@ -2,7 +2,6 @@
 using orchid_backend_net.Application.Common.Interfaces;
 using orchid_backend_net.Application.TaskAssign.UpdateTaskAssign;
 using orchid_backend_net.Application.TaskAttribute.UpdateTaskAttribute;
-using orchid_backend_net.Domain.Common.Exceptions;
 using orchid_backend_net.Domain.Entities;
 using orchid_backend_net.Domain.IRepositories;
 
@@ -16,7 +15,7 @@ namespace orchid_backend_net.Application.Tasks.UpdateTask
         public int? Status { get; set; }
         public List<UpdateTaskAssignCommand>? TaskAssignUpdate { get; set; }
         public List<UpdateTaskAttributeCommand>? TaskAttributeCommands { get; set; }
-        public UpdateTaskCommand(string id, string? name, string? description, 
+        public UpdateTaskCommand(string id, string? name, string? description,
             int? status, List<UpdateTaskAssignCommand>? assign, List<UpdateTaskAttributeCommand>? attribute)
         {
             ID = id;
@@ -28,8 +27,8 @@ namespace orchid_backend_net.Application.Tasks.UpdateTask
         }
     }
 
-    internal class UpdateTaskCommandHandler(ITaskRepository taskRepository, ITaskAssignRepository taskAssignRepository, 
-        ITaskAttributeRepository taskAttributeRepository ,ICurrentUserService currentUserService,
+    internal class UpdateTaskCommandHandler(ITaskRepository taskRepository, ITaskAssignRepository taskAssignRepository,
+        ITaskAttributeRepository taskAttributeRepository, ICurrentUserService currentUserService,
         ISender sender) : IRequestHandler<UpdateTaskCommand, string>
     {
         public async Task<string> Handle(UpdateTaskCommand request, CancellationToken cancellationToken)
@@ -44,17 +43,17 @@ namespace orchid_backend_net.Application.Tasks.UpdateTask
                 task.Update_by = currentUserService.UserId;
                 //check task assign list
                 var taskAssignList = await taskAssignRepository.FindAllAsync(x => x.TaskID.Equals(request.ID), cancellationToken);
-                foreach (var updateTaskAssignCommand in request.TaskAssignUpdate) 
-                { 
-                    if(taskAssignList.Any(x => x.ID.Equals(updateTaskAssignCommand.Id) && IsDifferentTechnician(x, updateTaskAssignCommand))) 
+                foreach (var updateTaskAssignCommand in request.TaskAssignUpdate)
+                {
+                    if (taskAssignList.Any(x => x.ID.Equals(updateTaskAssignCommand.Id) && IsDifferentTechnician(x, updateTaskAssignCommand)))
                         await sender.Send(updateTaskAssignCommand, cancellationToken);
                 }
 
                 //check task attribute list
                 var taskAttributeList = await taskAttributeRepository.FindAllAsync(x => x.TaskID.Equals(request.ID), cancellationToken);
-                foreach(var updateTaskAttributeCommand in request.TaskAttributeCommands)
+                foreach (var updateTaskAttributeCommand in request.TaskAttributeCommands)
                 {
-                    if(taskAttributeList.Any(x => x.ID.Equals(updateTaskAttributeCommand.Id) && IsDifferentAttribute(x, updateTaskAttributeCommand)))
+                    if (taskAttributeList.Any(x => x.ID.Equals(updateTaskAttributeCommand.Id) && IsDifferentAttribute(x, updateTaskAttributeCommand)))
                         await sender.Send(updateTaskAttributeCommand, cancellationToken);
                 }
                 return await taskRepository.UnitOfWork.SaveChangesAsync(cancellationToken) > 0 ? $"Updated task ID :{request.ID}" : $"Failed update task with ID :{request.ID}";
@@ -65,8 +64,8 @@ namespace orchid_backend_net.Application.Tasks.UpdateTask
             }
         }
 
-        private bool IsDifferentAttribute(TaskAttributes x, UpdateTaskAttributeCommand command) 
-            => x.Name != command.Name 
+        private bool IsDifferentAttribute(TaskAttributes x, UpdateTaskAttributeCommand command)
+            => x.Name != command.Name
             || x.MeasurementUnit != command.MeasurementUnit
             || x.Value != command.Value;
 
