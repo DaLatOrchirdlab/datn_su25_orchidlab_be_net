acb91119c2380f0578a34ee8349a908bbe488684
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2025-07-17 23:38:34 +0700

    fix: code simplifier

--- orchid-backend-net/orchid-backend-net.Application/Tasks/CreateTask/CreateTaskCommand.cs
+++ orchid-backend-net/orchid-backend-net.Application/Tasks/CreateTask/CreateTaskCommand.cs
@@ -63,8 +63,10 @@ namespace orchid_backend_net.Application.Tasks.CreateTask
 
                 foreach (var technicianID in request.TechnicianID)
                 {
-                    CreateTaskAssignCommand assignCommand = new(technicianID);
-                    assignCommand.TaskId = task.ID;
+                    CreateTaskAssignCommand assignCommand = new(technicianID)
+                    {
+                        TaskId = task.ID
+                    };
                     await sender.Send(assignCommand, cancellationToken);
                 }
 
