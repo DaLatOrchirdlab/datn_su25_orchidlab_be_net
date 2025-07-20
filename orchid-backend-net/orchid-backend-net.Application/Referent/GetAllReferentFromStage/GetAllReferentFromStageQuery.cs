eae488f48f6f515bd7ffce137e17f2d349188005
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2025-07-20 21:50:35 +0700

    feat: referent

--- orchid-backend-net/orchid-backend-net.Application/Referent/GetAllReferentFromStage/GetAllReferentFromStageQuery.cs
+++ orchid-backend-net/orchid-backend-net.Application/Referent/GetAllReferentFromStage/GetAllReferentFromStageQuery.cs
@@ -1,21 +1,22 @@
 ﻿using MediatR;
 using orchid_backend_net.Application.Common.Extension;
+using orchid_backend_net.Application.Common.Interfaces;
 using orchid_backend_net.Application.Common.Pagination;
 using orchid_backend_net.Domain.Entities;
 using orchid_backend_net.Domain.IRepositories;
 
 namespace orchid_backend_net.Application.Referent.GetAllReferentFromStage
 {
-    public class GetAllReferentFromStageCommand(string stageId, int pageNo, int pageSize) : IRequest<PageResult<ReferentDTO>>
+    public class GetAllReferentFromStageQuery(string stageId, int pageNo, int pageSize) : IRequest<PageResult<ReferentDTO>>, IQuery
     {
         public string StageId { get; set; } = stageId;
         public int PageNumber { get; set; } = pageNo;
         public int PageSize { get; set; } = pageSize;
     }
 
-    internal class GetAllReferentFromStageCommandHandler(IReferentRepository referentRepository) : IRequestHandler<GetAllReferentFromStageCommand, PageResult<ReferentDTO>>
+    internal class GetAllReferentFromStageQueryHandler(IReferentRepository referentRepository) : IRequestHandler<GetAllReferentFromStageQuery, PageResult<ReferentDTO>>
     {
-        public async Task<PageResult<ReferentDTO>> Handle(GetAllReferentFromStageCommand request, CancellationToken cancellationToken)
+        public async Task<PageResult<ReferentDTO>> Handle(GetAllReferentFromStageQuery request, CancellationToken cancellationToken)
         {
             try
             {
