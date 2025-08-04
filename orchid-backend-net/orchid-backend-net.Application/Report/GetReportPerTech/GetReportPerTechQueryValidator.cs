786e6fe481dd2d05e87a5830fa93aff30bd9b164
ThanhIsHereee <nquangthanh88@gmail.com>
2025-08-04 22:59:35 +0700

    feat: api filter by technicianID

---
+++ orchid-backend-net/orchid-backend-net.Application/Report/GetReportPerTech/GetReportPerTechQueryValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orchid_backend_net.Application.Report.GetReportPerTech
+{
+    public class GetReportPerTechQueryValidator : AbstractValidator<GetReportPerTechQuery>
+    {
+        public GetReportPerTechQueryValidator() 
+        {
+            
+        }
+
+        void Configuration()
+        {
+            RuleFor(x => x.techID)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Missing technicianID");
+        }
+    }
+}
