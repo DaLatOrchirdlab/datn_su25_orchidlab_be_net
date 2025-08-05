a7dc86a91f8d0aa55f80f61791d1a1ceb9e54812
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2025-08-05 12:46:34 +0700

    feat: disease command

---
+++ orchid-backend-net/orchid-backend-net.Application/Disease/Analysis/DiseaseAnalysisCommand.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using orchid_backend_net.Application.Common.Interfaces;
+using orchid_backend_net.Domain.Entities;
+using orchid_backend_net.Domain.IRepositories;
+
+namespace orchid_backend_net.Application.Disease.Analysis
+{
+    public class DiseaseAnalysisCommand : IRequest<OrchidAnalysisResult>, ICommand
+    {
+        public required byte[] ImageBytes { get; set; }
+    }
+
+    internal class DiseaseAnalysisCommandHandler(IOrchidAnalyzerService orchidAnalyzerService, IDiseaseRepository diseaseRepository) : IRequestHandler<DiseaseAnalysisCommand, OrchidAnalysisResult>
+    {
+        public async Task<OrchidAnalysisResult> Handle(DiseaseAnalysisCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                OrchidAnalysisResult result = await orchidAnalyzerService.AnalyzeAsync(request.ImageBytes);
+                var disease = await diseaseRepository.FindAsync(x => x.ID.Equals(result.Disease), cancellationToken);
+                return new OrchidAnalysisResult
+                {
+                    Stage = result.Stage,
+                    Disease = disease?.Name ?? "Unknown Disease"
+                };
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{ex.Message}");
+            }
+        }
+    }
+}
---
+++ orchid-backend-net/orchid-backend-net.Application/Disease/DiseaseDTO.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using orchid_backend_net.Application.Common.Mappings;
+using orchid_backend_net.Domain.Entities;
+
+namespace orchid_backend_net.Application.Disease
+{
+    public class DiseaseDTO : IMapFrom<Diseases>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Solution { get; set; }
+        public decimal InfectedRate { get; set; }
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Diseases, DiseaseDTO>()
+                .ReverseMap();
+        }
+    }
+}
