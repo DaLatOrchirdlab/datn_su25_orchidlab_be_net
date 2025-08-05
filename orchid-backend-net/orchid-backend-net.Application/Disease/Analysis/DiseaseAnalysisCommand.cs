8263b5407e03a1065087013c2995a6337ba4cdd0
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2025-08-05 15:16:16 +0700

    feat: diseasse api

--- orchid-backend-net/orchid-backend-net.Application/Disease/Analysis/DiseaseAnalysisCommand.cs
+++ orchid-backend-net/orchid-backend-net.Application/Disease/Analysis/DiseaseAnalysisCommand.cs
@@ -5,24 +5,21 @@ using orchid_backend_net.Domain.IRepositories;
 
 namespace orchid_backend_net.Application.Disease.Analysis
 {
-    public class DiseaseAnalysisCommand : IRequest<OrchidAnalysisResult>, ICommand
+    public class DiseaseAnalysisCommand : IRequest<AnalysisDTO>, ICommand
     {
         public required byte[] ImageBytes { get; set; }
     }
 
-    internal class DiseaseAnalysisCommandHandler(IOrchidAnalyzerService orchidAnalyzerService, IDiseaseRepository diseaseRepository) : IRequestHandler<DiseaseAnalysisCommand, OrchidAnalysisResult>
+    internal class DiseaseAnalysisCommandHandler(IOrchidAnalyzerService orchidAnalyzerService, IDiseaseRepository diseaseRepository) : IRequestHandler<DiseaseAnalysisCommand, AnalysisDTO>
     {
-        public async Task<OrchidAnalysisResult> Handle(DiseaseAnalysisCommand request, CancellationToken cancellationToken)
+        public async Task<AnalysisDTO> Handle(DiseaseAnalysisCommand request, CancellationToken cancellationToken)
         {
             try
             {
                 OrchidAnalysisResult result = await orchidAnalyzerService.AnalyzeAsync(request.ImageBytes);
-                var disease = await diseaseRepository.FindAsync(x => x.ID.Equals(result.Disease), cancellationToken);
-                return new OrchidAnalysisResult
-                {
-                    Stage = result.Stage,
-                    Disease = disease?.Name ?? "Unknown Disease"
-                };
+                var disease = await diseaseRepository.FindProjectToAsync<DiseaseDTO>(
+                    query => query.Where(disease => disease.ID.Equals(result.Disease)), cancellationToken);
+                return new AnalysisDTO(result.Stage, disease);
             }
             catch (Exception ex)
             {
---
+++ orchid-backend-net/orchid-backend-net.Application/Disease/AnalysisDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace orchid_backend_net.Application.Disease
+{
+    public class AnalysisDTO(string stage, DiseaseDTO disease)
+    {
+        public string Stage { get; set; } = stage;
+        public DiseaseDTO Disease { get; set; } = disease;
+    }
+}
