f88f62538e00569da9d6ffb8552d71c800952c3f
ThanhIsHereee <nquangthanh88@gmail.com>
2025-08-08 22:48:08 +0700

    bug/fix: stageName in taskTemplate & validator of task

--- orchid-backend-net/orchid-backend-net.Application/TaskTemplate/TaskTemplateDTO.cs
+++ orchid-backend-net/orchid-backend-net.Application/TaskTemplate/TaskTemplateDTO.cs
@@ -13,7 +13,7 @@ namespace orchid_backend_net.Application.TaskTemplate
     public class TaskTemplateDTO : IMapFrom<TaskTemplates>
     {
         public string Name { get; set; }
-        public string StageID { get; set; }
+        public string StageName { get; set; }
         public string Description { get; set; }
         public bool Status { get; set; }
         public List<TaskTemplateDetailsDTO> Details { get; set; }
@@ -22,6 +22,7 @@ namespace orchid_backend_net.Application.TaskTemplate
         {
             profile.CreateMap<TaskTemplates, TaskTemplateDTO>()
                 .ForMember(dest => dest.Details, opt => opt.MapFrom(src => src.TemplateDetails))
+                .ForMember(dest => dest.StageName, opt => opt.MapFrom(src => src.Stages.Name))
                 .ReverseMap();
         }
     }
