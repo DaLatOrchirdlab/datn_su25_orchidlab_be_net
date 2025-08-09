897b8e923787956c2bffea2bfbf021bdb290b8ee
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2025-08-09 10:29:02 +0700

    feat: infected sample command

---
+++ orchid-backend-net/orchid-backend-net.Application/InfectedSample/GetAll/GetAllInfectedSampleQuery.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using orchid_backend_net.Application.Common.Pagination;
+using orchid_backend_net.Domain.IRepositories;
+
+namespace orchid_backend_net.Application.InfectedSample.GetAll
+{
+    public class GetAllInfectedSampleQuery(int pageNo, int pageSize, string? sampleName, string? diseaseName) : IRequest<PageResult<InfectedSampleDTO>>
+    {
+        public int PageNumber { get; set; } = pageNo;
+        public int PageSize { get; set; } = pageSize;
+        public string? SampleName { get; set; } = sampleName;
+        public string? DiseaseName { get; set; } = diseaseName;
+    }
+
+    internal class GetAllInfectedSampleQueryHandler(IInfectedSampleRepository infectedSampleRepository, IDiseaseRepository diseaseRepository, ISampleRepository sampleRepository) : IRequestHandler<GetAllInfectedSampleQuery, PageResult<InfectedSampleDTO>>
+    {
+        public async Task<PageResult<InfectedSampleDTO>> Handle(GetAllInfectedSampleQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                throw new Exception($"{ex.Message}");
+            }
+        }
+    }
+}
---
+++ orchid-backend-net/orchid-backend-net.Application/InfectedSample/InfectedSampleDTO.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using orchid_backend_net.Application.Common.Mappings;
+using orchid_backend_net.Application.Disease;
+using orchid_backend_net.Application.Sample;
+using orchid_backend_net.Domain.Entities;
+using orchid_backend_net.Domain.Enums;
+
+namespace orchid_backend_net.Application.InfectedSample
+{
+    public class InfectedSampleDTO : IMapFrom<InfectedSamples>
+    {
+        public string ID { get; set; }
+        public SampleDTO Sample { get; set; }
+        public DiseaseDTO Disease { get; set; }
+        public string InfectedLevel { get; set; }
+        public string TreatmentStatus { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<InfectedSamples, InfectedSampleDTO>()
+                .ForMember(dest => dest.Sample, opt => opt.MapFrom(src => src.Sample))
+                .ForMember(dest => dest.Disease, opt => opt.MapFrom(src => src.Disease))
+                .ForMember(dest => dest.InfectedLevel,
+                    opt => opt.MapFrom(src => ((InfectedSampleInfectedLevel)src.InfectedLevel).ToString()))
+                .ForMember(dest => dest.TreatmentStatus,
+                    opt => opt.MapFrom(src => ((InfectedSampleTreatmentStatus)src.TreatmentStatus).ToString()));
+
+            profile.CreateMap<InfectedSampleDTO, InfectedSamples>()
+                .ForMember(dest => dest.InfectedLevel, opt => opt.MapFrom(src => (int)Enum.Parse<InfectedSampleInfectedLevel>(src.InfectedLevel)))
+                .ForMember(dest => dest.TreatmentStatus, opt => opt.MapFrom(src => (int)Enum.Parse<InfectedSampleTreatmentStatus>(src.TreatmentStatus)));
+        }
+    }
+}
