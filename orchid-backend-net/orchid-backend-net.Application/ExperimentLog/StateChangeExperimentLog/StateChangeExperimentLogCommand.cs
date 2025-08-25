3c4c0599658bf61497cca0b5735209a7c7e97b66
ThanhIsHereee <nquangthanh88@gmail.com>
2025-08-25 08:53:52 +0700

    update stateChange in linkeds

--- orchid-backend-net/orchid-backend-net.Application/ExperimentLog/StateChangeExperimentLog/StateChangeExperimentLogCommand.cs
+++ orchid-backend-net/orchid-backend-net.Application/ExperimentLog/StateChangeExperimentLog/StateChangeExperimentLogCommand.cs
@@ -13,20 +13,17 @@ namespace orchid_backend_net.Application.ExperimentLog.StateChangeExperimentLog
     public class StateChangeExperimentLogCommand : IRequest<string>, ICommand
     {
         public string ELID {  get; set; }
-        public string stageID { get; set; }
-        public string methodID { get; set; }
         public StateChangeExperimentLogCommand() { }
-        public StateChangeExperimentLogCommand(string eLID, string stageID, string methodID)
+        public StateChangeExperimentLogCommand(string ELID)
         {
-            ELID = eLID;
-            this.stageID = stageID;
-            this.methodID = methodID;
+            this.ELID = ELID;
         }
     }
 
     internal class StateChangeExperimentLogCommandHandler(IStageRepository stageRepository,
-        IMethodRepository methodRepository,
-        IExperimentLogRepository experimentLogRepository
+        ILinkedRepository linkedRepository,
+        IExperimentLogRepository experimentLogRepository,
+        ISampleRepository sampleRepository
         ) : IRequestHandler<StateChangeExperimentLogCommand, string>
     {
 
@@ -34,14 +31,28 @@ namespace orchid_backend_net.Application.ExperimentLog.StateChangeExperimentLog
         {
             try
             {
-                var stageStep = (await stageRepository.FindAsync(x => x.ID.Equals(request.stageID), cancellationToken))?.Step;
+                var EL = await experimentLogRepository.FindAsync(x => x.ID.Equals(request.ELID), cancellationToken);
+                var stageStep = (await stageRepository.FindAsync(x => x.ID.Equals(EL.CurrentStageID), cancellationToken))?.Step;
                 stageStep += 1;
-                var nextStage = await stageRepository.FindAsync(x => x.MethodID.Equals(request.methodID) && x.Step == stageStep, cancellationToken);
+                var nextStage = await stageRepository.FindAsync(x => x.MethodID.Equals(EL.MethodID) && x.Step == stageStep, cancellationToken);
                 if (nextStage == null)
                     throw new NotFoundException("Not found next stage, that is the last one.");
-                var EL = await experimentLogRepository.FindAsync(x => x.ID.Equals(request.ELID), cancellationToken);
+                List<Domain.Entities.Samples> sample = await sampleRepository.FindAllAsync(x => x.Linkeds.Any(x => x.ExperimentLogID.Equals(EL.ID)), cancellationToken);
+                foreach( var sampleItem in sample)
+                {
+                    linkedRepository.Add(new Domain.Entities.Linkeds()
+                    {
+                        ExperimentLogID = request.ELID,
+                        ProcessStatus = 0,
+                        StageID = nextStage.ID,
+                        SampleID = sampleItem.ID,
+                        TaskID = null
+                    });
+                }
+
                 EL.CurrentStageID = nextStage.ID;
                 experimentLogRepository.Update(EL);
+
                 return await experimentLogRepository.UnitOfWork.SaveChangesAsync(cancellationToken) > 0 ? "Successed" : "Failed";
             }
             catch (Exception ex) 
--- orchid-backend-net/orchid-backend-net.Application/ExperimentLog/StateChangeExperimentLog/StateChangeExperimentLogValidator.cs
+++ orchid-backend-net/orchid-backend-net.Application/ExperimentLog/StateChangeExperimentLog/StateChangeExperimentLogValidator.cs
@@ -11,12 +11,15 @@ namespace orchid_backend_net.Application.ExperimentLog.StateChangeExperimentLog
     public class StateChangeExperimentLogValidator : AbstractValidator<StateChangeExperimentLogCommand>
     {
         private readonly IExperimentLogRepository _experimentLogRepository;
-        private readonly IStageRepository _stageRepository;
-        private readonly IMethodRepository _methodRepository;
-        public StateChangeExperimentLogValidator(IStageRepository stageRepository, IMethodRepository methodRepository,IExperimentLogRepository experimentLogRepository)
+        //private readonly IStageRepository _stageRepository;
+        //private readonly IMethodRepository _methodRepository;
+        public StateChangeExperimentLogValidator(
+            //IStageRepository stageRepository, 
+            //IMethodRepository methodRepository,
+            IExperimentLogRepository experimentLogRepository)
         {
-            this._stageRepository = stageRepository;
-            this._methodRepository = methodRepository;
+            //this._stageRepository = stageRepository;
+            //this._methodRepository = methodRepository;
             this._experimentLogRepository = experimentLogRepository;
             Configuration();
         }
@@ -26,31 +29,31 @@ namespace orchid_backend_net.Application.ExperimentLog.StateChangeExperimentLog
                 .NotEmpty()
                 .NotNull()
                 .WithMessage("ExperimentLog ID can't null or empty");
-            RuleFor(x => x.stageID)
-                .NotEmpty()
-                .NotNull()
-                .WithMessage("Stage ID can't null or empty");
-            RuleFor(x => x.methodID)
-                .NotEmpty()
-                .NotNull()
-                .WithMessage("Method ID can't null or empty");
+            //RuleFor(x => x.stageID)
+            //    .NotEmpty()
+            //    .NotNull()
+            //    .WithMessage("Stage ID can't null or empty");
+            //RuleFor(x => x.methodID)
+            //    .NotEmpty()
+            //    .NotNull()
+            //    .WithMessage("Method ID can't null or empty");
             RuleFor(x => x.ELID)
                 .MustAsync(async (id, cancellationToken) => await IsExsitExperimentLog(id, cancellationToken))
                 .WithMessage(x => $"Not found ExperimentLog ID : {x.ELID}");
-            RuleFor(x => x.stageID)
-                .MustAsync(async (id, cancellationToken) => await IsExsitStage(id, cancellationToken))
-                .WithMessage(x => $"Not found Stage ID : {x.stageID}");
-            RuleFor(x => x.ELID)
-                .MustAsync(async (id, cancellationToken) => await IsExsitMethod(id, cancellationToken))
-                .WithMessage(x => $"Not found Method ID : {x.methodID}");
+            //RuleFor(x => x.stageID)
+            //    .MustAsync(async (id, cancellationToken) => await IsExsitStage(id, cancellationToken))
+            //    .WithMessage(x => $"Not found Stage ID : {x.stageID}");
+            //RuleFor(x => x.ELID)
+            //    .MustAsync(async (id, cancellationToken) => await IsExsitMethod(id, cancellationToken))
+            //    .WithMessage(x => $"Not found Method ID : {x.methodID}");
         
         }
 
         async Task<bool> IsExsitExperimentLog(string id, CancellationToken cancellationToken)
             => await _experimentLogRepository.AnyAsync(x => x.ID.Equals(id), cancellationToken);
-        async Task<bool> IsExsitStage(string id, CancellationToken cancellationToken)
-            => await _stageRepository.AnyAsync(x => x.ID.Equals(id), cancellationToken);
-        async Task<bool> IsExsitMethod(string id, CancellationToken cancellationToken)
-            => await _methodRepository.AnyAsync(x => x.ID.Equals(id), cancellationToken);
+        //async Task<bool> IsExsitStage(string id, CancellationToken cancellationToken)
+        //    => await _stageRepository.AnyAsync(x => x.ID.Equals(id), cancellationToken);
+        //async Task<bool> IsExsitMethod(string id, CancellationToken cancellationToken)
+        //    => await _methodRepository.AnyAsync(x => x.ID.Equals(id), cancellationToken);
     }
 }
