736f29b4ef75e8be13b909eea0da4d33dd3465ee
ThanhIsHereee <nquangthanh88@gmail.com>
2025-08-29 06:15:49 +0700

    feat: review report from technician

--- orchid-backend-net/orchid-backend-net.API/Controllers/Report/ReportController.cs
+++ orchid-backend-net/orchid-backend-net.API/Controllers/Report/ReportController.cs
@@ -8,6 +8,7 @@ using orchid_backend_net.Application.Report.CreateReport;
 using orchid_backend_net.Application.Report.DeleteReport;
 using orchid_backend_net.Application.Report.GetAllReport;
 using orchid_backend_net.Application.Report.GetReportInfor;
+using orchid_backend_net.Application.Report.ReviewReport;
 using orchid_backend_net.Application.Report.UpdateReport;
 using System.Net.Mime;
 
@@ -106,6 +107,32 @@ namespace orchid_backend_net.API.Controllers.Report
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<JsonResponse<string>>> Update(
             [FromBody] UpdateReportCommand command,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await sender.Send(command, cancellationToken);
+                logger.LogInformation("Received PUT request at {Time}", DateTime.UtcNow);
+                return Ok(new JsonResponse<string>(result));
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error occurred while processing PUT request at {Time}", DateTime.UtcNow);
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPut("review-report-change")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(JsonResponse<string>), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(JsonResponse<string>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<JsonResponse<string>>> ReviewReportChange(
+            [FromBody] ReviewReportCommand command,
             CancellationToken cancellationToken)
         {
             try
