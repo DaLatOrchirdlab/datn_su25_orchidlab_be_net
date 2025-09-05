d5def937d2ce901753d818ad77ecdebb1dc7c46f
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2025-09-05 17:41:43 +0700

    update: linked process status update

--- orchid-backend-net/orchid-backend-net.Infrastructure/Service/HostedService/ExperimentLogHostedService.cs
+++ orchid-backend-net/orchid-backend-net.Infrastructure/Service/HostedService/ExperimentLogHostedService.cs
@@ -140,7 +140,7 @@ namespace orchid_backend_net.Infrastructure.Service.HostedService
 
 
                 //update the linkeds into done
-                duplicatedFollowExperimentLogStageLinkeds.ForEach(x => x.ProcessStatus = 1);
+                duplicatedFollowExperimentLogStageLinkeds.ForEach(x => x.ProcessStatus = 2);
                 linkedRepo.UpdateRange(duplicatedFollowExperimentLogStageLinkeds);
 
                 //create new linkeds due to next stage
