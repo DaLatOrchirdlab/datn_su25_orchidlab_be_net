5b4309eb18c8d6cc4424a9e59b750711b057f2fe
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2025-09-14 16:03:51 +0700

    fix: create task

--- orchid-backend-net/orchid-backend-net.Application/TaskAssign/CreateTaskAssign/CreateTaskAssignCommand.cs
+++ orchid-backend-net/orchid-backend-net.Application/TaskAssign/CreateTaskAssign/CreateTaskAssignCommand.cs
@@ -8,7 +8,7 @@ namespace orchid_backend_net.Application.TaskAssign.CreateTaskAssign
     public class CreateTaskAssignCommand(string technicianId) : IRequest, ICommand
     {
         [JsonIgnore]
-        public string TaskId { get; set; }
+        public string? TaskId { get; set; }
         public string TechnicianId { get; set; } = technicianId;
     }
 
